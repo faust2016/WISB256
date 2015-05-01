eba5d75db001b9bb5002b813fb9ee55db22ae55f
faust2016 <faust2016@hotmail.com>
2015-05-01 14:53:18 +0000

    Nu echt goed

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -2,8 +2,9 @@ import time
 import sys
 #print('Number of arguments:', len(sys.argv), 'arguments.')
 #print('Argument List:', str(sys.argv))
+priemlijst = []
 def priemen(n): 
-    priemlijst = []
+    #priemlijst = []
     zeef = [True] * (n + 1)
     for p in range(2, n + 1):
         if zeef[p]:
@@ -15,8 +16,9 @@ def priemen(n):
        # bestand.write(str(num))
     bestand.write('\n'.join(str(num) for num in priemlijst))
     
+
 start = time.perf_counter()
 priemen(int(sys.argv[1]))
 eind = time.perf_counter()
 #print('Time required', eind - start, 'sec.')
-print('Found ' + len(priemlijst) + 'Prime numbers smaller than ' + str(sys.argv[1]) + 'in ' + str(eind-start) )
+print('Found ' + str(len(priemlijst)) + ' Prime numbers smaller than ' + str(sys.argv[1]) + 'in ' + str(eind-start) )
