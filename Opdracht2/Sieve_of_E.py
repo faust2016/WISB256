7e86189e36225941bcda7715bfec1cde9c44a8a4
faust2016 <faust2016@hotmail.com>
2015-05-01 14:34:44 +0000

    zou goed moeten zijn

--- Opdracht2/Sieve_of_E.py
+++ Opdracht2/Sieve_of_E.py
@@ -1,18 +1,22 @@
 import time
 import sys
-
+#print('Number of arguments:', len(sys.argv), 'arguments.')
+#print('Argument List:', str(sys.argv))
 def priemen(n): 
-    priemlijst, zeef = [], [True] * (n + 1)
+    priemlijst = []
+    zeef = [True] * (n + 1)
     for p in range(2, n + 1):
         if zeef[p]:
            priemlijst.append(p)
            for i in range(p**2, n + 1, p):
                zeef[i] = False
-    bestand = open('prime.dat', 'w')
-    for num in priemlijst:
-        bestand.write(str(num))
-
+    bestand = open(sys.argv[2], 'w')
+ # for num in priemlijst:
+       # bestand.write(str(num))
+    bestand.write('\n'.join(str(num) for num in priemlijst))
+    
 start = time.perf_counter()
-priemen(90)
+priemen(int(sys.argv[1]))
 eind = time.perf_counter()
-print('Time required', eind - start, 'sec.')
+#print('Time required', eind - start, 'sec.')
+print('Found ' + len(priemlijst) + 'Prime numbers smaller than ' + str(sys.argv[1]) + 'in ' + str(eind-start) )
