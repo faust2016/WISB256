613bbc3f73b069f3110e597d2b65cfbb50326ee1
faust2016 <faust2016@hotmail.com>
2015-05-21 14:32:40 +0000

    bisection klaar

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -17,9 +17,8 @@ def findRoot(f,a,b,epsilon):
 def findAllRoots(f,a,b,epsilon):
     output=[]
     for i in range(0,10):
-        temp=findRoot(f,a+(i/5)*(b-a),(b-(4-i)/5*(b-a)),epsilon)
+        temp=findRoot(f,a+(i/10)*(b-a),(b-(9-i)/10*(b-a)),epsilon)
         if temp!= None:
             output.append(temp)
-        print(str(a+(i/10)*(b-a))+' , '+ str(b-(9-i)/10*(b-a)))
     return output
     
--- Opdracht4/test.py
+++ Opdracht4/test.py
@@ -2,4 +2,4 @@ import bisection
 import math
 print(bisection.findRoot(lambda x:x*(x-1)-1,-1,3,.01))
 print(bisection.findAllRoots(lambda x:x*(x-1)-1,-1,3,.01))
-#print(bisection.findAllRoots(lambda x:x**2-2,-2,2,.01))
+print(bisection.findAllRoots(lambda x:x**2-2,-2,2,.01))
